34d975e2e0667726e2407816d73618dacbedfdab
Robert Varela <rvarela@utexas.edu>
2018-12-21 00:53:48 -0600

    Added cart icon to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,22 @@
 // @vendors
 import React from 'react';
+import IconButton from '@material-ui/core/IconButton';
+import Badge from '@material-ui/core/Badge';
+import { ShoppingCart } from '@material-ui/icons/';
+
 
 // @styles
 import './Header.scss';
 
-const Header = (props) => {
-    return (
-        <div className="header">
-            <figure className="header__logo" />
-        </div>
-    );
-};
+const Header = () => (
+    <div className="header">
+        <figure className="header__logo" />
+        <IconButton color="inherit">
+            <Badge badgeContent={4} color="secondary" invisible>
+                <ShoppingCart />
+            </Badge>
+        </IconButton>
+    </div>
+);
 
 export default Header;
