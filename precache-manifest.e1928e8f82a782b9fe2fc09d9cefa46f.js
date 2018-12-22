aeccbee7293cbf268e094c829e4b0cfac3326b16
Robert Varela <rvarela@utexas.edu>
2018-12-22 00:24:49 -0600

    Updates

---
+++ precache-manifest.e1928e8f82a782b9fe2fc09d9cefa46f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ebd8343bac35cf22a12d",
+    "url": "/pizza-planet/static/js/runtime~main.ebd8343b.js"
+  },
+  {
+    "revision": "1d7e5fd9e704694ebf12",
+    "url": "/pizza-planet/static/js/main.1d7e5fd9.chunk.js"
+  },
+  {
+    "revision": "338fb6b4b4e123775a69",
+    "url": "/pizza-planet/static/js/1.338fb6b4.chunk.js"
+  },
+  {
+    "revision": "1d7e5fd9e704694ebf12",
+    "url": "/pizza-planet/static/css/main.917d5686.chunk.css"
+  },
+  {
+    "revision": "0c450efb7c33886c95c92412f4327d8a",
+    "url": "/pizza-planet/index.html"
+  }
+];
