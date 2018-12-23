0ddd051417aee0888b610d016d669693b8fd92d7
Robert Varela <rvarela@utexas.edu>
2018-12-23 17:55:51 -0600

    Updates

--- precache-manifest.e67deda3226b03613aa80c0b6ea8b1e4.js
+++ precache-manifest.e67deda3226b03613aa80c0b6ea8b1e4.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/pizza-planet/static/js/runtime~main.ebd8343b.js"
   },
   {
-    "revision": "4589d7d31fb65d79ff4a",
-    "url": "/pizza-planet/static/js/main.4589d7d3.chunk.js"
+    "revision": "3f25f0bd8309e6f1ddc5",
+    "url": "/pizza-planet/static/js/main.3f25f0bd.chunk.js"
   },
   {
     "revision": "338fb6b4b4e123775a69",
     "url": "/pizza-planet/static/js/1.338fb6b4.chunk.js"
   },
   {
-    "revision": "4589d7d31fb65d79ff4a",
+    "revision": "3f25f0bd8309e6f1ddc5",
     "url": "/pizza-planet/static/css/main.917d5686.chunk.css"
   },
   {
-    "revision": "028306904ae4e46adf8a876fa776513c",
+    "revision": "a63f9e3dd1640a82e4228930499bbd39",
     "url": "/pizza-planet/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pizza-planet/precache-manifest.77a268f904e292b1df6b7ee902bd9fb4.js"
+  "/pizza-planet/precache-manifest.e67deda3226b03613aa80c0b6ea8b1e4.js"
 );
 
 workbox.clientsClaim();
